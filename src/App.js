98de4289353cd89bbf45b24fb994350e16bb4b29
Connor Tyrrell <connor@brewgene.com>
2021-12-05 15:45:26 -0500

    Added Placeholders for days 6-12

--- src/App.js
+++ src/App.js
@@ -12,6 +12,13 @@ import {
   Day3Page,
   Day4Page,
   Day5Page,
+  Day6Page,
+  Day7Page,
+  Day8Page,
+  Day9Page,
+  Day10Page,
+  Day11Page,
+  Day12Page,
 } from './pages';
 
 const App = () => {
@@ -36,6 +43,27 @@ const App = () => {
         </Route>
         <Route exact path="/5/:taskNumber">
           <Day5Page />
+        </Route>
+        <Route exact path="/6/:taskNumber">
+          <Day6Page />
+        </Route>
+        <Route exact path="/7/:taskNumber">
+          <Day7Page />
+        </Route>
+        <Route exact path="/8/:taskNumber">
+          <Day8Page />
+        </Route>
+        <Route exact path="/9/:taskNumber">
+          <Day9Page />
+        </Route>
+        <Route exact path="/10/:taskNumber">
+          <Day10Page />
+        </Route>
+        <Route exact path="/11/:taskNumber">
+          <Day11Page />
+        </Route>
+        <Route exact path="/12/:taskNumber">
+          <Day12Page />
         </Route>
       </Switch>
     </Router>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,13 @@ import Day2Page from './Day2/Day2Page';
 import Day3Page from './Day3/Day3Page';
 import Day4Page from './Day4/Day4Page';
 import Day5Page from './Day5/Day5Page';
+import Day6Page from './Day5/Day5Page';
+import Day7Page from './Day5/Day5Page';
+import Day8Page from './Day5/Day5Page';
+import Day9Page from './Day5/Day5Page';
+import Day10Page from './Day5/Day5Page';
+import Day11Page from './Day5/Day5Page';
+import Day12Page from './Day5/Day5Page';
 
 export {
     HomePage,
@@ -12,4 +19,11 @@ export {
     Day3Page,
     Day4Page,
     Day5Page,
+    Day6Page,
+    Day7Page,
+    Day8Page,
+    Day9Page,
+    Day10Page,
+    Day11Page,
+    Day12Page,
 };
